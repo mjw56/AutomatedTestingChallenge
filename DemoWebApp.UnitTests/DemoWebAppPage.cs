70722400e6c42c7ec9d47cba42f7891c2583a1d1
Mike Wilcox <mwilcox56@gmail.com>
2014-02-16 18:23:06 -0500

    add base driver class for selenium

---
+++ DemoWebApp.UnitTests/DemoWebAppPage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace DemoWebApp.UnitTests
+{
+    [Binding]
+    public class DemoWebAppPage
+    {
+        private static IWebDriver driver;
+
+        public static DemoWebAppPage NavigateTo(IWebDriver webDriver)
+        {
+            driver = webDriver;
+            driver.Navigate().GoToUrl("http://localhost:9098/Default.aspx");
+            var page = new DemoWebAppPage();
+            PageFactory.InitElements(driver, page);
+            return page;
+        }
+    }
+}
