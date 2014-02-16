307313cbce7c94a42efc0f0bc9c26021cdee314d
Mike Wilcox <mwilcox56@gmail.com>
2014-02-16 18:25:15 -0500

    remove dependencies not needed

--- DemoWebApp.UnitTests/DemoWebAppPage.cs
+++ DemoWebApp.UnitTests/DemoWebAppPage.cs
@@ -2,10 +2,6 @@
 using OpenQA.Selenium.Support.PageObjects;
 using OpenQA.Selenium.Support.UI;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TechTalk.SpecFlow;
 
 namespace DemoWebApp.UnitTests
