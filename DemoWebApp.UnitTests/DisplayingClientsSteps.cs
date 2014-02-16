dd9ee9b5d1a292d419ccd9d65d3b984e73f1da42
Mike Wilcox <mwilcox56@gmail.com>
2014-02-16 16:48:39 -0500

    add step definitions class

---
+++ DemoWebApp.UnitTests/DisplayingClientsSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace DemoWebApp.UnitTests
+{
+    [Binding]
+    public class DisplayingClientsSteps
+    {
+        [Given(@"That I have two clients defined")]
+public void GivenThatIHaveTwoClientsDefined()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [When(@"I load the page")]
+public void WhenILoadThePage()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Then(@"two clients should be displayed on the screen")]
+public void ThenTwoClientsShouldBeDisplayedOnTheScreen()
+{
+    ScenarioContext.Current.Pending();
+}
+    }
+}
