99f234299dada9a543d0453f4fd84308a219e102
Mike Wilcox <mwilcox56@gmail.com>
2014-02-16 23:29:33 -0500

    cleanup folder structure

---
+++ DemoWebApp.UnitTests/SpecFlow/DemoWebAppPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+
+namespace DemoWebApp.UnitTests
+{
+    [Binding]
+    public class DemoWebAppPage
+    {
+        private static IWebDriver driver;
+
+        public static DemoWebAppPage NavigateTo(IWebDriver webDriver)
+        {
+            driver = webDriver;
+            driver.Navigate().GoToUrl("http://localhost:9098/Default.aspx");
+            var page = new DemoWebAppPage();
+            PageFactory.InitElements(driver, page);
+            return page;
+        }
+    }
+}
---
+++ DemoWebApp.UnitTests/SpecFlow/Features/DisplayClients.feature
@@ -0,0 +1,12 @@
+﻿Feature: Data Displayed on Page
+
+@mytag
+Scenario: Display two clients
+	Given That I have two clients defined
+	When I load the page
+	Then two clients should be displayed on the screen
+
+Scenario: Two Clients Each Have a Name
+	Given That I have two clients defined
+	When I load the page
+	Then the two clients should each have a name
